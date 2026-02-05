d9aa41f04d135c159cd0c30428643b42e9cf3ed1
RedX <a.villa232925@simivalleyusd.org>
2026-02-05 09:08:39 -0800

    Delete frontend/js directory

--- frontend/js/programs/https.js
+++
@@ -1,9 +0,0 @@
-export function installHttpsProgram(eventBus, backend, terminal) {
-  eventBus.on("help:https", () => {
-    terminal.print("https.js: backend request relay available to programs.");
-  });
-
-  eventBus.on("help-extra", () => {
-    terminal.print("https.js loaded.");
-  });
-}
